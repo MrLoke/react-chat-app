5e19e505fd6e3402ebab1a9cc194bd07570947d1
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-02-16 03:32:53 +0100

    Add padding to search user input

--- src/components/UsersBar/SearchUserInput/SearchUserInputStyled.js
+++ src/components/UsersBar/SearchUserInput/SearchUserInputStyled.js
@@ -10,7 +10,7 @@ export const SearchUserContainer = styled.div`
 
 export const SearchInput = styled.input`
   width: 90%;
-  padding: 5px 10px;
+  padding: 10px 10px;
   margin-right: 5px;
   color: ${({ theme }) => theme.colors.dark};
   font-size: ${({ theme }) => theme.size.xs};
@@ -21,6 +21,6 @@ export const SearchInput = styled.input`
 
 export const SearchIcon = styled.div`
   position: absolute;
-  right: 10%;
-  top: 6%;
+  right: 13%;
+  top: 15%;
 `
