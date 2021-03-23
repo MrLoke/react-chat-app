b03e4fcc7eeda35e4d20b157b7505a2dd1108b44
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-03-23 16:01:05 +0100

    Add and implement lazy loading components

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,17 +1,19 @@
-import { useEffect } from 'react'
-import Home from 'pages/Home'
-import Chat from 'pages/Chat'
-import Signup from 'pages/Signup'
-import Login from 'pages/Login'
-import NotFound from 'pages/NotFound'
+import { useEffect, lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import LoadingSpinner from 'components/LoadingSpinner/LoadingSpinner'
 import { useDispatch } from 'react-redux'
 import { login, logout } from 'features/userSlice'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from 'theme/GlobalStyles'
 import { theme } from 'theme/theme'
 import { ModalProvider } from 'styled-react-modal'
-import { auth } from 'firebase-config'
+import { auth, db } from 'firebase-config'
+
+const Login = lazy(() => import('pages/Login'))
+const Signup = lazy(() => import('pages/Signup'))
+const Home = lazy(() => import('pages/Home'))
+const Chat = lazy(() => import('pages/Chat'))
+const NotFound = lazy(() => import('pages/NotFound'))
 
 const Root = () => {
   const dispatch = useDispatch()
@@ -19,6 +21,10 @@ const Root = () => {
   useEffect(() => {
     auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
+        db.collection('users')
+          .doc(auth.currentUser.uid)
+          .get()
+          .then((doc) => doc.ref.update({ status: 'online' }))
         dispatch(
           login({
             displayName: userAuth.displayName,
@@ -34,17 +40,19 @@ const Root = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <ModalProvider> {/* from styled-react-modal */}
-        <GlobalStyle />
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/channel/:id' component={Chat} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={Signup} />
-            <Route exact path='*' component={NotFound} />
-          </Switch>
-        </Router>
+      <ModalProvider>
+        <Suspense fallback={<LoadingSpinner fullScreen />}>
+          <GlobalStyle />
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/channel/:id' component={Chat} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+              <Route exact path='*' component={NotFound} />
+            </Switch>
+          </Router>
+        </Suspense>
       </ModalProvider>
     </ThemeProvider>
   )
