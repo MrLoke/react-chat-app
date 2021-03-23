c7411e20c921f5ea19331818ef42212ec4f7da18
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-03-23 16:14:10 +0100

    Display user status feature

--- src/components/UsersBar/UserInfo/UserInfo.js
+++ src/components/UsersBar/UserInfo/UserInfo.js
@@ -1,14 +1,19 @@
 import {
   UserContainer,
+  User,
   UserAvatar,
   UserName,
+  UserStatus,
 } from './UserInfoStyled'
 
 const UserInfo = ({ user }) => {
   return (
     <UserContainer>
-      <UserAvatar src={user.photoURL} alt='' />
-      <UserName>{user.displayName}</UserName>
+      <UserAvatar src={user.photoURL} alt={`${user.displayName} avatar`} />
+      <User>
+        <UserName>{user.displayName}</UserName>
+        <UserStatus status={user.status}>{user.status}</UserStatus>
+      </User>
     </UserContainer>
   )
 }
--- src/components/UsersBar/UserInfo/UserInfoStyled.js
+++ src/components/UsersBar/UserInfo/UserInfoStyled.js
@@ -4,6 +4,9 @@ export const UserContainer = styled.div`
   display: flex;
   align-items: center;
   margin: 10px 0 10px 15px;
+  ${({ theme }) => theme.media.xl} {
+    margin: 10px 0 10px 30px;
+  }
 `
 
 export const UserAvatar = styled.img`
@@ -14,10 +17,27 @@ export const UserAvatar = styled.img`
   cursor: pointer;
 `
 
+export const User = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
 export const UserName = styled.p`
   color: ${({ theme }) => theme.colors.light};
   font-size: ${({ theme }) => theme.size.s};
   cursor: pointer;
   font-weight: ${({ theme }) => theme.weight.medium};
   padding-left: 10px;
+`
+
+export const UserStatus = styled.p`
+  color: ${({ status, theme }) =>
+    status === 'online' ? theme.colors.green : theme.colors.secondaryShade};
+  font-size: ${({ theme }) => theme.size.xs};
+  cursor: pointer;
+  font-weight: ${({ theme }) => theme.weight.medium};
+  padding-left: 10px;
+  ::first-letter {
+    text-transform: uppercase;
+  }
 `
